d25ef4e11cb5aa3598392eac78a8294b023dec5b
Candelario <fcandela@9801a78fe00b.ant.amazon.com>
2017-03-07 17:12:21 -0500

    add path

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 eval 'which npm'
+PATH=$PATH:/root/.nvm/versions/node/v4.4.5/bin
 npm install unit.js -g
 npm install pm2 -g
--- start_application.sh
+++ start_application.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 eval 'which node'
+PATH=$PATH:/root/.nvm/versions/node/v4.4.5/bin
 pm2 start server.js
