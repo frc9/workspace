425b1c86e0d9d68a268bde16ce7db5ec54099297
Candelario <fcandela@9801a78fe00b.ant.amazon.com>
2017-03-07 15:28:42 -0500

    change to -g

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/root/.nvm/versions/node/v4.4.5/bin/npm install unit.js
-/root/.nvm/versions/node/v4.4.5/bin/npm install pm2
+/root/.nvm/versions/node/v4.4.5/bin/npm install unit.js -g
+/root/.nvm/versions/node/v4.4.5/bin/npm install pm2 -g
