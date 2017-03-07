c49e7e224405e99da5c786371da665621d2bfd8b
Candelario <fcandela@9801a78fe00b.ant.amazon.com>
2017-03-07 16:09:45 -0500

    change to relative paths for scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/root/.nvm/versions/node/v4.4.5/bin/npm install unit.js -g
-/root/.nvm/versions/node/v4.4.5/bin/npm install pm2 -g
+npm install unit.js -g
+npm install pm2 -g
--- stop_application.sh
+++ stop_application.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/root/.nvm/versions/node/v4.4.5/bin/pm2 stop server.js
+pm2 stop server.js
